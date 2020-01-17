6d3ce888e5acd95c21cd34aca384b0c6b5a658c1
Jhon Jairo <jjmuesesq@unal.edu.co>
2020-01-17 14:42:14 -0500

    app abogados funcional version 1

--- abogados/models.py
+++ abogados/models.py
@@ -15,7 +15,6 @@ from perfil.models import Perfil
 from origen_contacto.models import OrigenContacto
 from asesores.models import AsesoresDb
 
-
 class DbAbogados(models.Model):
     codigo = models.AutoField(primary_key=True)
     nombres = models.CharField(max_length=51)
@@ -24,11 +23,11 @@ class DbAbogados(models.Model):
     tarjeta_p = models.IntegerField(blank=True, null=True)
     fecha_nacimiento = models.DateField(blank=True, null=True)
     direccion = models.CharField(max_length=154, blank=True, null=True)
-    ciudad = models.ForeignKey(Municipio, db_column='ciudad', blank=True, null=True, related_name='DbAbogados.ciudad+', on_delete=models.PROTECT)
+    ciudad = models.ForeignKey(Municipio, db_column='ciudad', blank=True, null=True, on_delete=models.PROTECT, related_name='DbAbogados.ciudad+')
     ciudadnombre = models.CharField(db_column='ciudadNombre', max_length=27, blank=True, null=True)  # Field name made lowercase.
     departamento = models.CharField(max_length=18, blank=True, null=True)
     direccion2 = models.CharField(max_length=154, blank=True, null=True)
-    ciudad2 = models.ForeignKey(Municipio, db_column='ciudad2', blank=True, null=True, related_name='DbAbogados.ciudad2+', on_delete=models.PROTECT)
+    ciudad2 = models.ForeignKey(Municipio, db_column='ciudad2', blank=True, null=True, on_delete=models.PROTECT, related_name='DbAbogados.ciudad2+')
     perfil = models.ForeignKey(Perfil, db_column='perfil', blank=True, null=True, on_delete=models.PROTECT)
     empresa = models.CharField(max_length=56, blank=True, null=True)
     celular2 = models.CharField(max_length=15, blank=True, null=True)
@@ -45,12 +44,12 @@ class DbAbogados(models.Model):
     actualizacion = models.ForeignKey(AsesoresDb, db_column='actualizacion', blank=True, null=True, on_delete=models.PROTECT)
     observaciones = models.CharField(max_length=150, blank=True, null=True)
     fechaexpedicion = models.DateField(db_column='fechaExpedicion', blank=True, null=True)  # Field name made lowercase.
-    ciudadexpedicion = models.ForeignKey(Municipio, db_column='ciudadExpedicion', blank=True, null=True, related_name='DbAbogados.ciudadexpedicion+', on_delete=models.PROTECT)  # Field name made lowercase.
+    ciudadexpedicion = models.ForeignKey(Municipio, db_column='ciudadExpedicion', blank=True, null=True, on_delete=models.PROTECT)  # Field name made lowercase.
     genero = models.ForeignKey(Genero, db_column='genero', on_delete=models.PROTECT)
 
     class Meta:
         managed = True
-        db_table = 'db_abogados'         
+        db_table = 'db_abogados'
         verbose_name = 'Abogado'
         verbose_name_plural = 'Informe de DB_ABOGADOS' 
         ordering = ["codigo"]
